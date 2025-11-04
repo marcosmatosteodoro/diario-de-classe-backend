e2d17a1868428616f3146707f224017d73e09dab
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-04 01:22:31 -0300

    feat: update user routes to use userRouter and change endpoint to /professores

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { healthCheck, welcome } from './controllers/healthController.js';
-import userRoutes from './routes/users.js';
+import userRoutes from './routes/userRouter.js';
 
 const userController = userRoutes;
 
@@ -8,6 +8,6 @@ const router = express.Router();
 
 router.get('/', welcome);
 router.get('/health', healthCheck);
-router.use('/api/users', userController);
+router.use('/professores', userController);
 
 export default router;
