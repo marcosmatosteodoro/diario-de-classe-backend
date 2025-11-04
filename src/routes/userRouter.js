4fe8e0741f9f06065317dfca051e7785104a84e8
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-04 01:53:02 -0300

    feat: add route for retrieving user by ID in userRouter

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { createUser } from '../controllers/usersController.js';
 import { GetUserListController } from '../controllers/user/getUserListController.js';
+import { GetUserController } from '../controllers/user/getUserController.js';
 
 const router = express.Router();
 
 // GET /api/users - Buscar todos os usuários
 router.get('/', GetUserListController.handle);
+
+// GET /api/users/:id - Buscar usuário por ID
+router.get('/:id', GetUserController.handle);
 
 // POST /api/users - Criar novo usuário
 router.post('/', createUser);
