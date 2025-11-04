fcc340ad9286fe8a156a1a4316b5531b95c37614
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-04 20:53:54 -0300

    feat: add DeleteUserController import and route for user deletion

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createUser } from '../controllers/usersController.js';
 import { GetUserListController } from '../controllers/user/getUserListController.js';
 import { GetUserController } from '../controllers/user/getUserController.js';
+import { DeleteUserController } from '../controllers/user/deleteUserController.js';
 import { validateId } from '../middlewares/validateId.js';
 
 const router = express.Router();
@@ -14,5 +15,8 @@ router.get('/:id', validateId, GetUserController.handle);
 
 // POST /api/users - Criar novo usuário
 router.post('/', createUser);
+
+// DELETE /api/users/:id - Deletar usuário por ID
+router.delete('/:id', validateId, DeleteUserController.handle);
 
 export default router;
