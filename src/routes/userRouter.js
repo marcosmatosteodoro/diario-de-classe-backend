4bc9a81ee4912409679441043e978acf6c870c39
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-05 09:22:17 -0300

    docs: add comments for controllers and validation middlewares in userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
+// Controladores
 import { GetUserListController } from '../controllers/user/getUserListController.js';
 import { GetUserController } from '../controllers/user/getUserController.js';
 import { DeleteUserController } from '../controllers/user/deleteUserController.js';
 import { CreateUserController } from '../controllers/user/createUserController.js';
 import { UpdateUserController } from '../controllers/user/updateUserController.js';
+// Middlewares de validação
 import { validateId } from '../middlewares/validateId.js';
 import { validateCreateUser } from '../middlewares/validateCreateUser.js';
 import { validateUpdateUser } from '../middlewares/validateUpdateUser.js';
