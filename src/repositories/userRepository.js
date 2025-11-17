ee67a38e92f1c5f3a8edc0fd7688c6a700a3dd95
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-17 06:49:04 -0300

    feat: add 'disponibilidades' field to select fields in UserRepository

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -20,6 +20,7 @@ export default class UserRepository extends AbstractRepository {
       senha: false,
       resetarSenha: true,
       permissao: true,
+      disponibilidades: true,
       dataCriacao: true,
       dataAtualizacao: true
     };
