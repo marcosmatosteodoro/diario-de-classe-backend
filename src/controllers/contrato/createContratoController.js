94019c81d32a32b45340b26558a21595745a43ee
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-19 09:49:42 -0300

    fix: testando existência do aluno antes de continuar

--- src/controllers/contrato/createContratoController.js
+++ src/controllers/contrato/createContratoController.js
@@ -2,6 +2,7 @@ import AbstractController from '../abstractController.js';
 import { CreateContratoService } from '../../services/contrato/createContratoService.js';
 import { IsContratoAlunoAtivoExistsService } from '../../services/contrato/isContratoAlunoAtivoExistsService.js';
 import { IsContratoAlunoPendenteExistsService } from '../../services/contrato/isContratoAlunoPendenteExistsService.js';
+import { GetAlunoService } from '../../services/aluno/getAlunoService.js';
 
 export class CreateContratoController extends AbstractController {
   constructor(req, res) {
@@ -11,6 +12,14 @@ export class CreateContratoController extends AbstractController {
 
   async execute() {
     try {
+      const aluno = await GetAlunoService.handle(this.idAluno);
+
+      if (!aluno) {
+        return this.res.status(404).json({
+          message: this.req.t('alunos.get.not_found')
+        });
+      }
+
       if (await IsContratoAlunoAtivoExistsService.handle(this.idAluno)) {
         return this.res.status(400).json({
           message: this.req.t('contratos.create.aluno_ativo_exists')
