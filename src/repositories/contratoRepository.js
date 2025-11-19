7a500f9b8f2795067c3229fbc0eb46a91fce4716
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-19 06:39:24 -0300

    feat: criação do repositório de contrato

---
+++ src/repositories/contratoRepository.js
@@ -0,0 +1,31 @@
+import prisma from '../db/prisma.js';
+import AbstractRepository from './abstractRepository.js';
+
+/**
+ * Repository para operações com contratos
+ * Centraliza todas as operações de banco de dados relacionadas aos contratos
+ */
+export default class ContratoRepository extends AbstractRepository {
+  getEntity() {
+    return prisma.contrato;
+  }
+
+  getSelectFields() {
+    return {
+      id: true,
+      idDoAluno: true,
+      dataDeInicio: true,
+      dataDeTermino: true,
+      status: true,
+      totalAulas: true,
+      totalAulasFeitas: true,
+      totalReposicoes: true,
+      totalFaltas: true,
+      totalAulasCanceladas: true,
+      dataCriacao: true,
+      dataAtualizacao: true,
+      aluno: false,
+      diaAulas: false
+    };
+  }
+}
