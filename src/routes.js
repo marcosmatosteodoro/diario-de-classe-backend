5a50c1c44aec8af520f6042b0022aeb1117dce8c
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-20 08:36:46 -0300

    feat: registro do endpoint get alunos-aulas

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import userRoutes from './routes/userRouter.js';
 import alunoRouters from './routes/alunoRouter.js';
 import configuracaoRouter from './routes/configuracaoRouter.js';
 import contratoRouter from './routes/contratoRouter.js';
+import diaAulaRouter from './routes/diaAulaRouter.js';
 
 const authController = authRouter;
 const userController = userRoutes;
 const alunoController = alunoRouters;
 const configuracaoController = configuracaoRouter;
 const contratoController = contratoRouter;
+const diaAulaController = diaAulaRouter;
 
 const router = express.Router();
 
@@ -21,5 +23,6 @@ router.use('/professores', userController);
 router.use('/alunos', alunoController);
 router.use('/configuracao', configuracaoController);
 router.use('/contratos', contratoController);
+router.use('/dias-aulas', diaAulaController);
 
 export default router;
