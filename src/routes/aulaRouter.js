db0b3f072f52b1116b4f11d6bb84c246b992660c
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-22 07:04:23 -0300

    feat: criação da rota /api/aulas:id get

--- src/routes/aulaRouter.js
+++ src/routes/aulaRouter.js
@@ -1,12 +1,12 @@
 import express from 'express';
 // Controladores
 import { GetAulaListController } from '../controllers/aula/getAulaListController.js';
-// import { GetAulaController } from '../controllers/aula/getAulaController.js';
+import { GetAulaController } from '../controllers/aula/getAulaController.js';
 // import { DeleteAulaController } from '../controllers/aula/deleteAulaController.js';
 // import { CreateAulaController } from '../controllers/aula/createAulaController.js';
 // import { UpdateAulaController } from '../controllers/aula/updateAulaController.js';
 // Middlewares de validação
-// import { validateId } from '../middlewares/validateId.js';
+import { validateId } from '../middlewares/validateId.js';
 // import { validateCreateAula } from '../middlewares/aula/validateCreateAula.js';
 // import { validateUpdateAula } from '../middlewares/aula/validateUpdateAula.js';
 import { validateSearchQuery } from '../middlewares/validateSearchQuery.js';
@@ -17,7 +17,7 @@ const router = express.Router();
 router.get('/', validateSearchQuery, GetAulaListController.handle);
 
 // GET /api/aulas/:id - Buscar aula por ID
-// router.get('/:id', validateId, GetAulaController.handle);
+router.get('/:id', validateId, GetAulaController.handle);
 
 // POST /api/aulas - Criar novo aula
 // router.post('/', validateCreateAula, CreateAulaController.handle);
