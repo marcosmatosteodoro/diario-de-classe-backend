8d3db7a53028a40f9ed7bcaeb3502bc1ba121938
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-22 07:46:44 -0300

    feat: criação da rota /api/aulas:id put

--- src/routes/aulaRouter.js
+++ src/routes/aulaRouter.js
@@ -4,11 +4,11 @@ import { GetAulaListController } from '../controllers/aula/getAulaListController
 import { GetAulaController } from '../controllers/aula/getAulaController.js';
 import { DeleteAulaController } from '../controllers/aula/deleteAulaController.js';
 import { CreateAulaController } from '../controllers/aula/createAulaController.js';
-// import { UpdateAulaController } from '../controllers/aula/updateAulaController.js';
+import { UpdateAulaController } from '../controllers/aula/updateAulaController.js';
 // Middlewares de validação
 import { validateId } from '../middlewares/validateId.js';
 import { validateCreateAula } from '../middlewares/aula/validateCreateAula.js';
-// import { validateUpdateAula } from '../middlewares/aula/validateUpdateAula.js';
+import { validateUpdateAula } from '../middlewares/aula/validateUpdateAula.js';
 import { validateSearchQuery } from '../middlewares/validateSearchQuery.js';
 
 const router = express.Router();
@@ -23,7 +23,7 @@ router.get('/:id', validateId, GetAulaController.handle);
 router.post('/', validateCreateAula, CreateAulaController.handle);
 
 // PUT /api/aulas/:id - Atualizar aula por ID
-// router.put('/:id', validateId, validateUpdateAula, UpdateAulaController.handle);
+router.put('/:id', validateId, validateUpdateAula, UpdateAulaController.handle);
 
 // DELETE /api/aulas/:id - Deletar aula por ID
 router.delete('/:id', validateId, DeleteAulaController.handle);
