914f2b6ab2e6b39ff343e37ac6431cff4ce918ba
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2026-01-13 10:58:18 -0300

    feat: add environment and constants logging for better debugging

--- src/server.js
+++ src/server.js
@@ -4,6 +4,24 @@ import app from './app.js';
 import Constants from './utilities/constants.js';
 import i18n from './utilities/i18n.js';
 
+console.log('Environment Check:', {
+  NODE_ENV: process.env.NODE_ENV,
+  HAS_DATABASE_URL: !!process.env.DATABASE_URL,
+  HAS_JWT_SECRET: !!process.env.JWT_SECRET_TOKEN
+});
+
+console.log('Constants check:', {
+  env: Constants.env,
+  envCheck: Constants.envCheck,
+  isProduction: Constants.isProduction,
+  serviceName: Constants.serviceName,
+  port: Constants.port,
+  jwtSecret: Constants.jwtSecret,
+  jwtRefreshSecret: Constants.jwtRefreshSecret,
+  accessExp: Constants.accessExp,
+  refreshExp: Constants.refreshExp
+});
+
 // Aguardar inicialização do i18n antes de iniciar o servidor
 i18n.on('initialized', () => {
   app.listen(Constants.port || 3000, () => {
